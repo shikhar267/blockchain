eece622818257475fd6a2c6fc0e034d35369ba76
unknown <shikharsingh267@gmail.com>
2022-08-02 07:52:29 -0700

    commited

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.15;
+
+contract HelloWorld
+{
+
+    // truffle development
+    //  migrate
+    //  var hw
+    // HelloWorld.deployed().then(function (deployed){hw=deployed;});
+    // hw.SayHello.call()
+     function SayHello() public pure returns (string memory)
+    {
+        return "Hello World";
+    }
+}
